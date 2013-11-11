6f9792ab15be1c76d3067371b891a1955076f7d6
thekungf00bunny <phillips.ev@gmail.com>
2013-11-11 17:36:21 -0600

    still won't work
    removed the auto join and changed 'woot' to '#woot'

--- script.js
+++ script.js
@@ -53,51 +53,18 @@ function initListeners() {
     currentsong = API.getMedia();
     djs = API.getDJs();
      rand();
-     setTimeout(function(){ document.getElementById('woot').click(); },randtimeout);
+     setTimeout(function(){ document.getElementById('#woot').click(); },randtimeout);
         
         API.addEventListener(API.DJ_ADVANCE, function( obj ) {
                 djs = API.getDJs();
                 currentsong = API.getMedia();
                 rand();
-                setTimeout(function(){ document.getElementById('woot').click(); },randtimeout);
-        
-                if (autoqueue && ($('div#button-dj-quit').css('display')=='none') && ($('div#button-dj-waitlist-leave').css('display')=='none')) {
-                     addtoQueue();
+                setTimeout(function(){ document.getElementById('#woot').click(); },randtimeout);
                 }
         });
 }
 
-determineAutoqueue();
-if (autoqueue && ($('div#button-dj-quit').css('display')=='none') && ($('div#button-dj-waitlist-leave').css('display')=='none')) {
-    addtoQueue();
-}
-initListeners();
-
-function rand(){
-    randtimeout = Math.random() * 10;
-    randtimeout = Math.round(randtimeout * 1000);
-    return randtimeout;
-}
-
 var updatePrefs;
-
-$.getScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js').done(function(){
-    $('head').append("<link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery.ui.all.css' type='text/css'/>");
-    
-    determineAutoqueue = function() {
-        var input = prompt("Would you like to enable auto-queueing?", "yes/no  (not case sensitive)").toLowerCase();
-        if (input == "yes") {
-                autoqueue = true;
-                
-        }
-        else if (input == "no") {
-                autoqueue = false;
-        } 
-        else   {
-                alert("You didn't validly tell me whether you want to enable auto-queueing or not... please run the script again..");     
-        }                                 
-                
-    }
 
         //getOutsideSettings();
     
