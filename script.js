e74d42feeee7b48d39e18bf3ffa2054c1cd2b969
thekungf00bunny <phillips.ev@gmail.com>
2013-11-11 19:31:04 -0600

    Update script.js
    API.on(API.DJ_ADVANCE, function(){setTimeout(function(){ $("#woot").click();},3000)});

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ plug.2autowoot
 //THE FANCY NEW plud.dj autowoot maybe
 
 API.chatLog("Luna! script ready for action!", true);
+
+document.getElementById('woot').click(); 
+API.on(API.DJ_ADVANCE, callback); 
+function callback(obj) { 
+document.getElementById('woot').click(); 
+};
+var RMEnhanced = new RMEnhancedModel;
